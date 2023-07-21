66f91e837bdf82cda2e45cf30631e9620e92741a
Pratik Shringarpure <114896501+de1uze@users.noreply.github.com>
2023-07-21 20:15:08 +0530

    Update Home.js

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -4,8 +4,8 @@ function Home() {
   return (
     <>
       
-      <h1>Welcome to open CRM</h1>
-      <img src='https://thumbs.dreamstime.com/b/crm-customer-relationship-management-business-internet-techology-concept-137888377.jpg' alt='opencrm'/>
+      <h1>Welcome to CRM</h1>
+      <img src='https://media.istockphoto.com/id/1205243544/vector/crm-customer-relationship-management-concept-clients-support-software-and-online-marketing.jpg?s=612x612&w=0&k=20&c=xKKC4e9HKsu3heGMk9i5MyPW2zfwWMOWU0AfU4NGXds=' alt='opencrm'/>
     </>
   );
 }
