3788239afb364179ea5bf5ca5d9edaa1de45da31
Pratik Shringarpure <114896501+de1uze@users.noreply.github.com>
2023-07-21 21:17:09 +0530

    Update Home.js

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -5,7 +5,7 @@ function Home() {
     <>
       
       <h1>Welcome to CRM</h1>
-      <img src='https://media.istockphoto.com/id/1205243544/vector/crm-customer-relationship-management-concept-clients-support-software-and-online-marketing.jpg?s=612x612&w=0&k=20&c=xKKC4e9HKsu3heGMk9i5MyPW2zfwWMOWU0AfU4NGXds=' alt='opencrm'/>
+      <img src='https://www.anodius.com/wp-content/uploads/2022/05/what-is-crm-system-anodius.jpg.webp' alt='opencrm'/>
     </>
   );
 }
